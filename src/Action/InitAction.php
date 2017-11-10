cf4e63dfcbde84ed34d33d591e6d4f8275b643fb
Rob Meijer <rob.meijer@immediate.co.uk>
2017-11-10 11:58:58 +0000

    Remove priority and arguments properties from example Action

--- src/Action/InitAction.php
+++ src/Action/InitAction.php
@@ -7,22 +7,6 @@ use IM\Fabric\Package\WordPress\Action;
 class InitAction extends Action
 {
     /**
-     * The priority property already has a default value of 10.
-     * This property can omitted if the priority is the same.
-     *
-     * @var int
-     */
-    protected $priority = 10;
-
-    /**
-     * The arguments property already has a default value of 1.
-     * This property can be omitted if the number of arguments is the same.
-     *
-     * @var int
-     */
-    protected $arguments = 1;
-
-    /**
      * @param array ...$args
      *
      * @return mixed
