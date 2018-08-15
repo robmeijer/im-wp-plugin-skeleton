56775bef4f24efccba683f1865fd3f94f671ee18
Rob Meijer <rob.meijer@immediate.co.uk>
2018-08-15 14:03:32 +0100

    Remove call to vendor/autoload and call WP directly for activation hooks

--- im-plugin-skeleton.php
+++ im-plugin-skeleton.php
@@ -13,12 +13,6 @@ if (! defined('ABSPATH')) {
     return;
 }
 
-// If you're not using Composer for your whole project, but only the plugin,
-// you can include the following to autoload the required classes.
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require_once __DIR__ . '/vendor/autoload.php';
-}
-
 // The following lines of code allows you to specify a local '/views' directory
 // Uncomment these lines if you wish to include views in the plugin
 //if (! class_exists(\Timber\Timber::class)) {
@@ -31,7 +25,7 @@ $plugin = new IM\Fabric\Plugin\PluginSkeleton\PluginSkeletonPlugin();
 
 // If you want to register activation and de-activation hooks, you can uncomment the following lines
 // Make sure that your plugin has the required 'activate' and 'deactivate' methods
-//$plugin->wordPress->registerActivationHook(__FILE__, [$plugin, 'activate']);
-//$plugin->wordPress->registerDeactivationHook(__FILE__, [$plugin, 'deactivate']);
+//register_activation_hook(__FILE__, [$plugin, 'activate']);
+//register_deactivation_hook(__FILE__, [$plugin, 'deactivate']);
 
 $plugin->run();
